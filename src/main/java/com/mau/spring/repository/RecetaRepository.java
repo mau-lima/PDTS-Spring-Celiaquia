3a615a0182f8e6b02b3e618ff802fb82e50e2af8
Mauricio Lima <limamauricio303@gmail.com>
2020-12-29 12:01:25 -0300

    Agrega endpoints sobre receta

--- src/main/java/com/mau/spring/repository/RecetaRepository.java
+++ src/main/java/com/mau/spring/repository/RecetaRepository.java
@@ -4,6 +4,9 @@ import com.mau.spring.model.Receta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecetaRepository  extends JpaRepository<Receta,Integer> {
+    List<Receta> findByNombre(String nombre);
 }
--- src/test/java/com/mau/spring/ApplicationTests.java
+++ src/test/java/com/mau/spring/ApplicationTests.java
@@ -1,7 +1,13 @@
 package com.mau.spring;
 
+import io.restassured.RestAssured;
+import io.restassured.matcher.RestAssuredMatchers;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.restassured.RestAssured.*;
+import 		io.restassured.matcher.RestAssuredMatchers.*;
+import  org.hamcrest.Matchers.*;
 
 @SpringBootTest
 class ApplicationTests {
@@ -10,4 +16,10 @@ class ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void prueba1() {
+		//este ejemplo no prueba nada por ahora.
+		RestAssured.get("/events?id=390").then().statusCode(200).assertThat()
+				.body("data.leagueId", Matchers.equalTo(35));
+	}
 }
