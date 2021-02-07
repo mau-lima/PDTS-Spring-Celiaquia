ef967e08876e3d16024d533aac06d0fb65851427
Mauricio Lima <limamauricio303@gmail.com>
2021-02-07 17:50:35 -0300

    Crea la clase usuario y el enum de frecuenciaGluten

---
+++ src/main/java/com/mau/spring/model/Usuario.java
@@ -0,0 +1,32 @@
+package com.mau.spring.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+    private String nombre;
+    private String apellido;
+    private Date fecha_de_nacimiento;
+    private boolean es_celiaco;
+    private Date fecha_diagnostico_celiaquia;
+    private boolean en_tratamiento_medico;
+    private boolean sigue_dieta_sin_gluten;
+    private boolean come_carnes;
+    private boolean come_verduras;
+    private boolean come_frutas;
+    private ZonedDateTime fecha_ultima_actividad;
+    private FrecuenciaGluten frecuencia_ingestion_gluten;
+}
